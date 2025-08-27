bdc221f267a4ff971f0a1b21553f0237e0432205
Axelwhite1 <165157295+Axelwhite1@users.noreply.github.com>
2025-08-27 12:11:32 -0300

    PROYECTO DE JUEGO (2025)
    miembros:axel

--- (axel,joaquin)proyecto-de-juego/main.gd
+++ (axel,joaquin)proyecto-de-juego/main.gd
@@ -20,12 +20,16 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$hud.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	$hud.update_score(score)
 	$hud.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_mobtimer_timeout() -> void:
 	# Create a new instance of the Mob scene.
